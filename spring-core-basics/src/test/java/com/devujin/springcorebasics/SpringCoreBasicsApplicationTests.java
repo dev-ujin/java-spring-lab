88a3704084f2eece314f7a0bcece3c5d9c145cc1
YOUJIN LEE <youjin.lee.dev@gmail.com>
2022-07-30 22:52:54 +0200

    chore: Setup initial project with Spring initializer

---
+++ spring-core-basics/src/test/java/com/devujin/springcorebasics/SpringCoreBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devujin.springcorebasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
